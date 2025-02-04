295f7ce2f38754b47ae0dfe8d455712a658feb66
Wittchen <wittchenchris@gmail.com>
2025-02-04 13:10:49 +0100

    Changed file name

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
-import { initModels } from './models/initialize.js';
+import { initModels } from './models/.initialize.js';
 
 dotenv.config();
 
