5eb9f15263fc06f0eb168ad94f5cb3b7e8487df9
Wittchen <wittchenchris@gmail.com>
2025-02-18 11:21:51 +0100

    Created user controller & service

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,59 @@
+import UserService from '../services/user.service.js';
+
+class UserController {
+    static async create(req, res) {
+        const { firstName, lastName, email } = req.body;
+        if (![firstName, lastName, email].every(Boolean)) {
+            return res.sendStatus(400);
+        }
+        await UserService.create({ firstName, lastName, email });
+        return res.sendStatus(201);
+    }
+
+    static async getAll(req, res) {
+        const users = await UserService.getAll();
+        return res.status(200).json(users);
+    }
+
+    static async getByEmail(req, res) {
+        const user = await UserService.getByEmail(req.params.email);
+        if (!user) { return res.sendStatus(404); }
+        return res.status(200).json(user);
+    }
+
+    static async update(req, res) {
+        const { firstName, lastName, email, accountStatus } = req.body;
+        if (![firstName, lastName, email, accountStatus].every(Boolean)) {
+            return res.sendStatus(400);
+        }
+        const updatedUser = await UserService.update(req.params.id, { firstName, lastName, email, accountStatus });
+        if (!updatedUser) { return res.sendStatus(404); }
+        return res.sendStatus(204);
+    }
+
+    static async delete(req, res) {
+        const deleted = await UserService.delete(req.params.id);
+        if (!deleted) { return res.sendStatus(404); }
+        return res.sendStatus(204);
+    }
+
+    static async login(req, res) {
+        const { email, password } = req.body;
+        if (![email, password].every(Boolean)) {
+            return res.sendStatus(400);
+        }
+        const result = await UserService.login(email, password);
+        return res.status(result.code).json({ status: result.status });
+    }
+
+    static async changePassword(req, res) {
+        const { newPassword } = req.body;
+        if (![newPassword].every(Boolean)) {
+            return res.sendStatus(400);
+        }
+        const result = await UserService.changePassword(req.params.email, newPassword);
+        return res.status(result.code).json({ status: result.status });
+    }
+}
+
+export default UserController;
---
+++ backend/src/services/user.service.js
@@ -0,0 +1,54 @@
+import User from '../models/user.model.js';
+import { ACCOUNT_STATUS } from '../utils/constants.js';
+import PasswordManager from '../utils/password.js';
+
+class UserService {
+    async create(data) {
+        return await User.create(data);
+    }
+
+    async getAll() {
+        return await User.findAll();
+    }
+
+    async getByEmail(email) {
+        return await User.findOne({ where: { email } });
+    }
+
+    async update(id, data) {
+        const [updated] = await User.update(data, { where: { id } });
+        return updated ? await this.getByEmail(data.email) : null;
+    }
+
+    async delete(id) {
+        return await User.update({
+            firstName: "Unknown User",
+            lastName: "",
+            email: "",
+            password: ""
+        },{ where: { id } });
+    }
+
+    async login(email, password) {
+        const user = await this.getByEmail(email);
+        if (!user || (user.password && !PasswordManager.verify(password, user.password))) {
+            return { code: 400, status: "invalid_credentials" };
+        }
+        if (user.accountStatus !== ACCOUNT_STATUS.ACTIVE) {
+            return { code: 403, status: user.accountStatus };
+        }
+        return { code: 200, status: "success", user: user };
+    }
+
+    async changePassword(email, newPassword) {
+        const user = await this.getByEmail(email);
+        if (!user || user.accountStatus !== ACCOUNT_STATUS.CHANGE_PASSWORD) {
+            return { code: 403, status: "not_allowed" };
+        }
+        const hashedPassword = PasswordManager.hash(newPassword);
+        await user.update({ password: hashedPassword, accountStatus: ACCOUNT_STATUS.ACTIVE });
+        return { code: 200, status: "success", user: user };
+    }
+}
+
+export default new UserService();
